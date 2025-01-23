f609853e0d5b000bf13745635f70f432c2971f0c
Manish Jha <manishjha962@gmail.com>
2025-01-23 11:34:58 +0530

    update database connection path in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 
-const db = require('./configs/')
+const db = require('./configs/mongoose-connection')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
